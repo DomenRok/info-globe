940779f0368c68308e3bc8a8956f95eb80c4a241
mariobm <mario.balukcic1@gmail.com>
2020-11-07 11:26:49 +0100

    Basic algolia autocomplete (#10)
    Co-authored-by: Mario Balukcic <mario.balukcic@lucis.si>

--- frontend/dragonhack-sinergise/src/main.js
+++ frontend/dragonhack-sinergise/src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router';
 import App from './App.vue'
+import PlacesPage from "./PlacesPage";
+Vue.use(VueRouter);
 
 Vue.config.productionTip = false
 
+const router = new VueRouter({
+  routes: [
+    { path: '/places', name: 'Places', component: PlacesPage },
+  ]
+})
+
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
