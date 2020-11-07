aca3fe7eb4035b198521ecc35b16d6aec35fc168
mariobm <mb1616@student.uni-lj.si>
2020-11-08 00:21:55 +0100

    Fixed and rewrite query for city and country (#24)

--- frontend/dragonhack-sinergise/src/components/Info/SPARQLQueryDispatcher.js
+++ frontend/dragonhack-sinergise/src/components/Info/SPARQLQueryDispatcher.js
@@ -1,20 +1,48 @@
-export class SPARQLQueryDispatcher {
+export default class SPARQLQueryDispatcher {
 	constructor() {
 		this.endpoint = 'https://query.wikidata.org/sparql';
     }
     
-    getPopulation(name) {
-        const sparqlQuery = `SELECT ?population WHERE {
-                                SERVICE wikibase:mwapi {
-                                    bd:serviceParam mwapi:search "${name}" .    
-                                    bd:serviceParam mwapi:language "en" .    
-                                    bd:serviceParam wikibase:api "EntitySearch" .
-                                    bd:serviceParam wikibase:endpoint "www.wikidata.org" .
-                                    bd:serviceParam wikibase:limit 1 .
-                                    ?item wikibase:apiOutputItem mwapi:item .
-                                }
-                                ?item wdt:P1082 ?population
-                            }`;
+    getCountryInfo(name) {
+        const sparqlQuery = `SELECT ?population ?countryLabel ?geoshape ?area ?capitalLabel ?flag ?gdp WHERE {
+            SERVICE wikibase:mwapi {
+                bd:serviceParam mwapi:search "${name}" .    
+                bd:serviceParam mwapi:language "en" .    
+                bd:serviceParam wikibase:api "EntitySearch" .
+                bd:serviceParam wikibase:endpoint "www.wikidata.org" .
+                bd:serviceParam wikibase:limit 1 .
+                ?item wikibase:apiOutputItem mwapi:item .
+            }
+            SERVICE wikibase:label { bd:serviceParam wikibase:language "en" }
+            ?item wdt:P1082 ?population;
+                  wdt:P17 ?country;
+                  wdt:P3896 ?geoshape;
+                  wdt:P2046 ?area;
+                  wdt:P36 ?capital;
+                  wdt:P41 ?flag;
+                  wdt:P4010 ?gdp
+          }`;
+        
+        return this.query( sparqlQuery );
+    }
+
+    getCityInfo(name) {
+        const sparqlQuery = `SELECT ?population ?area ?countryLabel ?flag ?geoshape WHERE {
+            SERVICE wikibase:mwapi {
+                bd:serviceParam mwapi:search "${name}" .    
+                bd:serviceParam mwapi:language "en" .    
+                bd:serviceParam wikibase:api "EntitySearch" .
+                bd:serviceParam wikibase:endpoint "www.wikidata.org" .
+                bd:serviceParam wikibase:limit 1 .
+                ?item wikibase:apiOutputItem mwapi:item .
+            }
+            SERVICE wikibase:label { bd:serviceParam wikibase:language "en" }
+            ?item wdt:P1082 ?population;
+                  wdt:P17 ?country;
+                  wdt:P41 ?flag;
+                  wdt:P2046 ?area
+            OPTIONAL { ?item wdt:P3896 ?geoshape   }
+          }`;
         
         return this.query( sparqlQuery );
     }
