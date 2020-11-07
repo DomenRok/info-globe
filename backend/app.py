79b3b74939b8c3cceaca733fcc4b42ec0cd6e50c
Domen Bruncek <domen@arvio.si>
2020-11-07 13:54:13 +0100

    remove unused templates

--- backend/app.py
+++ backend/app.py
@@ -19,15 +19,6 @@ def hello():
     hits = (redis.get('hits'))
     return "This site has been peep'd {} times.".format(hits)
 
-#@app.route('/api/autocomplete/<city_name>')
-def city_autocomplete():
-    pass
-
-def geocode():
-    pass
-
-def reverse_geocode():
-    pass
 
 @app.route('/api/city/<city_name>')
 def wikipedia(city_name):
@@ -44,10 +35,12 @@ def wikipedia(city_name):
     page_id = api_response['query']['search'][0]['pageid']
     return wikipedia_content(page_id)
 
+
 def wikipedia_content(page_id):
     uri = f"http://en.wikipedia.org/w/api.php?action=query&prop=info&pageids={page_id}&inprop=url&format=json"
     request = requests.get(uri)
     return jsonify(request.json()['query']['pages'][str(page_id)])
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
